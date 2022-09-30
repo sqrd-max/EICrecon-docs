c9a4aba40e648ac781bb8e1409756dd1e4e7bfa8
Dmitry Romanov <romanovda@gmail.com>
2022-09-30 15:10:55 -0400

    Fixed gatsbi site

---
+++ src/@rocketseat/gatsby-theme-docs/components/Logo.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+// Path to the logo file on your project
+import rocketseatLogo from '../../../../static/banner.png';
+
+const Logo = () => {
+  //console.warn("here!");
+  return (<img src={rocketseatLogo} alt="Rocketseat logo" style={{ width: 180 }} />);
+};
+
+export default Logo;
